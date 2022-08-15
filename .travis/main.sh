8eb1b752fa447a63a5eac2fd54a1deee79a122ac
leto1210+Github <github@cparla.fr>
2022-08-15 18:07:57 +0200

    Update main.sh

--- .travis/main.sh
+++ .travis/main.sh
@@ -33,7 +33,7 @@ setup_dependencies() {
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update -y
-  sudo apt install -y docker-ce=20.10.7
+  sudo apt install -y docker-ce=20.10.13~3-0~ubuntu
 
   docker info
 }

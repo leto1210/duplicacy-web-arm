ce3ff416c4dd490481a453ac7dfa4f35e8c40516
leto1210+Github <github@cparla.fr>
2022-08-16 18:13:40 +0200

    Update main.sh

--- .travis/main.sh
+++ .travis/main.sh
@@ -32,7 +32,7 @@ setup_dependencies() {
   cp ./.travis/default.json /tmp/default.json
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  #$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update -y
   #sudo apt upgrade docker-ce -y
   sudo apt install -y docker-ce=19.03.9

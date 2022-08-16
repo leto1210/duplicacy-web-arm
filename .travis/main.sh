ec57cf3cc67c4fbd5983193edc4922b06d826436
leto1210+Github <github@cparla.fr>
2022-08-16 18:12:37 +0200

    Update main.sh

--- .travis/main.sh
+++ .travis/main.sh
@@ -27,11 +27,11 @@ setup_dependencies() {
   echo "INFO:
   Setting up dependencies.
   "
-  #sudo mkdir -p /etc/apt/keyrings
-  #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  #cp ./.travis/default.json /tmp/default.json
-  #echo \
-  #"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  cp ./.travis/default.json /tmp/default.json
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   #$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update -y
   #sudo apt upgrade docker-ce -y
